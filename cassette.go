201d56bcf481dbfee2be6251f2101b8ada6f83af
Tillman Mosley III <tmosley@dermpathlab.com>
2015-03-05 19:23:40 -0500

    Cassette Changes

--- cassette.go
+++ cassette.go
@@ -6,14 +6,15 @@ import (
 )
 
 type Cassette struct {
-	Id       bson.ObjectId `bson:",omitempty" json:",omitempty"`
-	QRCode   string        `bson:"QRCode,omitempty" json:"QRCode,omitempty"`
-	SN       string        `bson:"SN,omitempty" json:"SN,omitempty"`
-	CaseID   string        `bson:"caseID,omitempty" json:"caseID,omitempty"`
-	Gross    *CasGross     `bson:"gross,omitempty" json:"gross,omitempty"`
-	Pieces   string        `bson:"pieces,omitempty" json:"pieces,omitempty"`
-	Specimen string        `bson:"specimen,omitempty" json:"specimen,omitempty"`
-	Tissue   *Tissue       `bson:"tissue,omitempty" json:"tissue,omitempty"`
+	Id        bson.ObjectId `bson:",omitempty" json:",omitempty"`
+	QRCode    string        `bson:"QRCode,omitempty" json:"QRCode,omitempty"`
+	SN        string        `bson:"SN,omitempty" json:"SN,omitempty"`
+	CaseID    string        `bson:"caseID,omitempty" json:"caseID,omitempty"`
+	Gross     *CasGross     `bson:"gross,omitempty" json:"gross,omitempty"`
+	Embedding *Embedding    `bson:"embedding,omitempty" json:"embedding,omitempty"`
+	Pieces    string        `bson:"pieces,omitempty" json:"pieces,omitempty"`
+	Specimen  string        `bson:"specimen,omitempty" json:"specimen,omitempty"`
+	Tissue    *Tissue       `bson:"tissue,omitempty" json:"tissue,omitempty"`
 }
 
 type CasGross struct {
@@ -21,6 +22,10 @@ type CasGross struct {
 	Date    int    `bson:"date,omitempty" json:"date,omitempty"`
 }
 
+type Embedding struct {
+	Account string `bson:"account,omitempty" json:"account,omitempty"`
+	Date    int    `bson:"date,omitempty" json:"date,omitempty"`
+}
 type Tissue struct {
 	Account string `bson:"account,omitempty" json:"account,omitempty"`
 	Date    int    `bson:"date,omitempty" json:"date,omitempty"`
